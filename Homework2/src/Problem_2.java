3a788ac8f8099c4aa9c03b61320ece1f8d967977
FaaizN <fzndm1@gmail.com>
2021-09-14 16:59:42 -0500

    Created a char array to store user input and checked indices to determine if the input was a valid SSN

--- Homework2/src/Problem_2.java
+++ Homework2/src/Problem_2.java
@@ -2,17 +2,30 @@ import java.util.Scanner;
 
 public class Problem_2 {
 
-    public static boolean checkSSN(String SSN, boolean check)
+    public static boolean checkSSN(char[] SSN, boolean check)
     {
-
+        //Check if the string is in the proper format for a SSN
+        if(SSN.length == 11)
+        {
+            //Check if the chracter has hyphens in the proper place
+            if(Character.isDigit(SSN[3]) && Character.isDigit(SSN[6]))
+            {
+                check = false;
+            }
+            else //Hyphens in proper place and the SSN is valid
+            {
+                check = true;
+            }
+        }
+        else //SSN was not the proper length
+        {
+            check = false;
+        }
         return check;
     }
 
     public static void main(String[] args)
     {
-        //Create String to store SSN
-        String SSN = null;
-
         //Create boolean to determine if String is SSN (TRUE) or not (FALSE)
         boolean isSSN = true;
 
@@ -20,19 +33,30 @@ public class Problem_2 {
         Scanner input = new Scanner(System.in);
 
         //Prompt the user for input and store input into String object "SSN" using Scanner object "input"
-        System.out.println("Enter a SSN: ");
-        SSN = input.nextLine();
+        System.out.print("Enter a SSN: ");
+        char[] SSN = input.next().toCharArray();
 
-        //Call the checkSSN function
+        //Call the check SSN function
         isSSN = checkSSN(SSN, isSSN);
 
+        //If SSN is true, output that the SSN is valid, else output that SSN is invalid
         if(isSSN == true)
         {
-            System.out.println(SSN + " is a valid social security number");
+            for(int i = 0; i < SSN.length; i++)
+            {
+                System.out.print(SSN[i]);
+            }
+
+            System.out.println(" is a valid social security number");
         }
         else
         {
-            System.out.println(SSN + " is an invalid social security number");
+            for(int i = 0; i < SSN.length; i++)
+            {
+                System.out.print(SSN[i]);
+            }
+
+            System.out.println(" is an invalid social security number");
         }
     }
 }
