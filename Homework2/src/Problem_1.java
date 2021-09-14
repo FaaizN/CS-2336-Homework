5a5c932c5b442c49c4adb9e76f5ee71baac95abc
FaaizN <fzndm1@gmail.com>
2021-09-14 18:25:34 -0500

    Cannot use methods, rewrote homework in accordance to chapter 1-5

--- Homework2/src/Problem_1.java
+++ Homework2/src/Problem_1.java
@@ -2,58 +2,6 @@ import java.util.Scanner;
 
 public class Problem_1 {
 
-    //This function assigns the days of the month to an int depending on the
-    //user input
-    public static int Count(String input, int year, int numDays)
-    {
-        //This switch statement assigns numDays the number of days in a month depending on the Month the user entered
-        switch(input)
-        {
-            case "Jan":
-                numDays = 31;
-                break;
-            case "Feb":
-                //If the year is divisible by 4 then numDays is 29 because of leap year
-                if (year % 4 == 0)
-                    numDays = 29;
-                else
-                    numDays = 28;
-                break;
-            case "Mar":
-                numDays = 31;
-                break;
-            case "Apr":
-                numDays = 30;
-                break;
-            case "May":
-                numDays = 31;
-                break;
-            case "Jun":
-                numDays = 30;
-                break;
-            case "Jul":
-                numDays = 31;
-                break;
-            case "Aug":
-                numDays = 31;
-                break;
-            case "Sep":
-                numDays = 30;
-                break;
-            case "Oct":
-                numDays = 31;
-                break;
-            case "Nov":
-                numDays = 30;
-                break;
-            case "Dec":
-                numDays = 31;
-                break;
-        }
-
-        return numDays;
-    }
-
     //Display the output to the console
     public static void output(int numDays, String month, int year)
     {
@@ -77,10 +25,69 @@ public class Problem_1 {
         System.out.print("Enter a month: ");
         month = userMonth.nextLine();
 
-        //Call the Count function
-        numDays = Count(month, year, numDays);
+        //Check user input for month
+        if(month.length() > 3)
+        {
+            System.out.println("Incorrect input. Please only enter the first 3 letters of the month with the" +
+                    " first one in uppercase");
+        }
+        else
+        {
+            if(Character.isUpperCase(month.charAt(0)) == false)
+            {
+                System.out.println("Incorrect input. Please only enter the first 3 letters of the month with the" +
+                        " first one in uppercase");
+            }
+            else
+            {
+                //This switch statement assigns numDays the number of days in a month depending on the Month the user entered
+                switch(month)
+                {
+                    case "Jan":
+                        numDays = 31;
+                        break;
+                    case "Feb":
+                        //If the year is divisible by 4 then numDays is 29 because of leap year
+                        if (year % 4 == 0)
+                            numDays = 29;
+                        else
+                            numDays = 28;
+                        break;
+                    case "Mar":
+                        numDays = 31;
+                        break;
+                    case "Apr":
+                        numDays = 30;
+                        break;
+                    case "May":
+                        numDays = 31;
+                        break;
+                    case "Jun":
+                        numDays = 30;
+                        break;
+                    case "Jul":
+                        numDays = 31;
+                        break;
+                    case "Aug":
+                        numDays = 31;
+                        break;
+                    case "Sep":
+                        numDays = 30;
+                        break;
+                    case "Oct":
+                        numDays = 31;
+                        break;
+                    case "Nov":
+                        numDays = 30;
+                        break;
+                    case "Dec":
+                        numDays = 31;
+                        break;
+                }
+                //Output the solution
+                System.out.println(month + " " + year + " has " + numDays + " days");
+            }
+        }
 
-        //Call the output function
-        output(numDays, month, year);
     }
 }
