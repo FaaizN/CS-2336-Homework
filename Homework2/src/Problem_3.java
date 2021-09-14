2fdced82d0bdd9823a348d663bd1b0db1d787321
FaaizN <fzndm1@gmail.com>
2021-09-14 17:16:26 -0500

    Added user input

--- Homework2/src/Problem_3.java
+++ Homework2/src/Problem_3.java
@@ -1,2 +1,27 @@
+import java.util.Scanner;
+
 public class Problem_3 {
+
+    public static void main(String[] args)
+    {
+        //Create the variables needed for this program
+        String userName;
+        double hours, payRate, fedTax,
+               stateTax, grossPay, netPay;
+
+        //Create a scanner object to read in user input;
+        Scanner input = new Scanner(System.in);
+
+        //Prompt user for input and store in variables
+        System.out.print("Enter employee's name: ");
+        userName = input.nextLine();
+        System.out.print("Enter number of hours worked in a week: ");
+        hours = input.nextDouble();
+        System.out.print("Enter hourly pay rate: ");
+        payRate = input.nextDouble();
+        System.out.println("Enter federal tax withholding rate: ");
+        fedTax = input.nextDouble();
+        System.out.println("Enter state tax withholding rate:");
+        stateTax = input.nextDouble();
+    }
 }
