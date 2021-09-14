7fc2feeba39dd638e2aef4e9e898217460053166
FaaizN <fzndm1@gmail.com>
2021-09-14 18:12:18 -0500

    Cannot use methods or arrays, rewrote homework in accordance to chapter 1-5

--- Homework2/src/Problem_2.java
+++ Homework2/src/Problem_2.java
@@ -2,61 +2,41 @@ import java.util.Scanner;
 
 public class Problem_2 {
 
-    public static boolean checkSSN(char[] SSN, boolean check)
-    {
-        //Check if the string is in the proper format for a SSN
-        if(SSN.length == 11)
-        {
-            //Check if the chracter has hyphens in the proper place
-            if(Character.isDigit(SSN[3]) && Character.isDigit(SSN[6]))
-            {
-                check = false;
-            }
-            else //Hyphens in proper place and the SSN is valid
-            {
-                check = true;
-            }
-        }
-        else //SSN was not the proper length
-        {
-            check = false;
-        }
-        return check;
-    }
-
     public static void main(String[] args)
     {
-        //Create boolean to determine if String is SSN (TRUE) or not (FALSE)
-        boolean isSSN = true;
-
         //Create a Scanner object to take user input
         Scanner input = new Scanner(System.in);
 
         //Prompt the user for input and store input into String object "SSN" using Scanner object "input"
         System.out.print("Enter a SSN: ");
-        char[] SSN = input.next().toCharArray();
+        String SSN = input.nextLine();
 
-        //Call the check SSN function
-        isSSN = checkSSN(SSN, isSSN);
-
-        //If SSN is true, output that the SSN is valid, else output that SSN is invalid
-        if(isSSN == true)
+        //Check if the SSN is valid
+        if(SSN.length() == 11)
         {
-            for(int i = 0; i < SSN.length; i++)
+            if(SSN.charAt(3) != '-' && SSN.charAt(6) != '-')
             {
-                System.out.print(SSN[i]);
+                System.out.println(SSN + " is an invalid social security number");
             }
-
-            System.out.println(" is a valid social security number");
+            else
+            {
+                //Check if all other character index are digits or not
+                if(Character.isDigit(SSN.charAt(0)) && Character.isDigit(SSN.charAt(1)) &&Character.isDigit(SSN.charAt(2))
+                        && Character.isDigit(SSN.charAt(4)) && Character.isDigit(SSN.charAt(5)) &&
+                        Character.isDigit(SSN.charAt(7)) && Character.isDigit(SSN.charAt(8)) &&
+                        Character.isDigit(SSN.charAt(9)) && Character.isDigit(SSN.charAt(10)))
+                {
+                    System.out.println(SSN + " is a valid social security number");
+                }
+                else
+                {
+                    System.out.println(SSN + " is an invalid social security number");
+                }
+            }
         }
         else
         {
-            for(int i = 0; i < SSN.length; i++)
-            {
-                System.out.print(SSN[i]);
-            }
-
-            System.out.println(" is an invalid social security number");
+            System.out.println(SSN + " is an invalid social security number");
         }
     }
 }
