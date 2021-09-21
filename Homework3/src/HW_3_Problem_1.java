6f1b7f5d4054a1d2f9edeadb5dcbb48bc34ea89f
FaaizN <fzndm1@gmail.com>
2021-09-21 18:23:55 -0500

    Created methods for program

---
+++ Homework3/src/HW_3_Problem_1.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class HW_3_Problem_1 {
+
+    public static boolean isValid(long number)
+    {
+        return true;
+    }
+
+    public static int sumOfDoubleEvenPlace(long number)
+    {
+        return Math.toIntExact(number);
+    }
+
+    public static int getDigit(int number)
+    {
+        return number;
+    }
+
+    public static int sumOfOddPlace(long number)
+    {
+        return Math.toIntExact(number);
+    }
+
+    public static boolean prefixMatched(long number, int d)
+    {
+        return false;
+    }
+
+    public static int getSize(long d)
+    {
+        return Math.toIntExact(d);
+    }
+
+    public static long getPrefix(long number, int k)
+    {
+        return number;
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world");
+    }
+}
