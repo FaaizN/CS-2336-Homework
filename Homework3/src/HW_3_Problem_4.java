4b3acb18172b54daab483b8f8ab5b2cf91f822b1
FaaizN <fzndm1@gmail.com>
2021-09-25 19:38:25 -0500

    Completed HW

--- Homework3/src/HW_3_Problem_4.java
+++ Homework3/src/HW_3_Problem_4.java
@@ -4,25 +4,25 @@ public class HW_3_Problem_4 {
 
     public static boolean isSorted(int[] list)
     {
-        boolean isSorted = true;
+        boolean sort = true;
 
         //Go through list and if current index is greater than next index return false
         for(int i = 0; i < list.length - 1; i++)
         {
             if(list[i] > list[i + 1])
             {
-               isSorted = false;
+               sort = false;
                break;
             }
         }
-        return isSorted;
+        return sort;
     }
 
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
         int size;
-        System.out.println("Enter list: ");
+        System.out.print("Enter list: ");
 
         //Take the first number from the list and make it the size of the next list
         int[] list = new int[input.nextInt()];
